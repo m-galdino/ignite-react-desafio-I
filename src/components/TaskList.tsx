6b50a8405b6b8b238b8aa2595bad2f31ba6c27d7
Mauricio Galdino <mauricio.galdino@versatecnologia.com.br>
2021-04-18 12:38:13 -0300

    implementando as funcionalidades

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {
+      let array = tasks
+
+      array.push({
+        id: array.length ? array.length : 0,
+        title: newTaskTitle,
+        isComplete: false
+      })
+
+      setTasks(array)
+      setNewTaskTitle("")
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    let array = tasks.filter(value => value.id != id)
+    setTasks(array)
   }
 
   return (
